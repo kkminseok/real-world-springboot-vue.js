537502bcf395dadcaa224d0190d977aefadadd47
kms <aaaa727@khu.ac.kr>
2022-09-07 23:25:17 +0900

    feat : postgres connect

---
+++ src/main/java/com/io/realworld/PostgreSQLRunner.java
@@ -0,0 +1,35 @@
+package com.io.realworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+
+@Component
+public class PostgreSQLRunner implements ApplicationRunner {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            System.out.println(dataSource.getClass());
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE t_product(product_no INTEGER NOT NULL, product_name VARCHAR(255), PRIMARY KEY (product_no))";
+            statement.executeUpdate(sql);
+        }
+        jdbcTemplate.execute("INSERT INTO t_product VALUES (1, 'Big shirt')");
+    }
+}
