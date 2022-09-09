948fe5a296758250384d1625e1b9d1866798d5d8
kms <aaaa727@khu.ac.kr>
2022-09-09 16:24:24 +0900

    feat : User Service divide by Interface and Implement

---
+++ src/main/java/com/io/realworld/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.io.realworld.service;
+
+import com.io.realworld.DTO.UserSignupRequest;
+import com.io.realworld.repository.User;
+import com.io.realworld.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User signup(UserSignupRequest userSignupRequest) {
+        return userRepository.save(User.of(userSignupRequest.getUsername(),
+                userSignupRequest.getEmail(),
+                userSignupRequest.getPassword()));
+    }
+
+}
