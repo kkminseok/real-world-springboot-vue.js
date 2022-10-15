722d18bf0c5e9da80454e1d3188244d5a705b155
kms <aaaa727@khu.ac.kr>
2022-10-15 14:47:15 +0900

    merge

---
+++ src/main/java/com/io/realworld/domain/aggregate/profile/entity/Profile.java
@@ -0,0 +1,6 @@
+package com.io.realworld.domain.aggregate.profile.entity;
+
+public class Profile {
+
+}
+
---
+++ src/main/java/com/io/realworld/domain/aggregate/profile/service/ProfilesService.java
@@ -0,0 +1,13 @@
+package com.io.realworld.domain.aggregate.profile.service;
+
+import com.io.realworld.domain.aggregate.profile.dto.Profile;
+import com.io.realworld.domain.aggregate.user.dto.UserAuth;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfilesService {
+
+    public Profile getProfile(String username, UserAuth userAuth){
+        return null;
+    }
+}
