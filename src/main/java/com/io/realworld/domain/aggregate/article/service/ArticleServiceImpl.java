92cd19da1027aeaaa8b3df75e2a795ffd80ec0f0
kms <aaaa727@khu.ac.kr>
2022-10-16 23:48:10 +0900

    fix : rename articleDto `Article` -> `Articledto` because confuse Article Entity Name

--- src/main/java/com/io/realworld/domain/aggregate/article/service/ArticleServiceImpl.java
+++ src/main/java/com/io/realworld/domain/aggregate/article/service/ArticleServiceImpl.java
@@ -1,6 +1,6 @@
 package com.io.realworld.domain.aggregate.article.service;
 
-import com.io.realworld.domain.aggregate.article.dto.Article;
+import com.io.realworld.domain.aggregate.article.dto.Articledto;
 import com.io.realworld.domain.aggregate.article.dto.ResponseArticle;
 import com.io.realworld.domain.aggregate.user.dto.UserAuth;
 import org.springframework.stereotype.Service;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class ArticleServiceImpl implements ArticleService{
 
     @Override
-    public ResponseArticle createArticle(UserAuth userAuth, Article article) {
+    public ResponseArticle createArticle(UserAuth userAuth, Articledto article) {
         return null;
     }
 
