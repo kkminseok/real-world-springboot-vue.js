0f9fcf38bfc4333a920253a62be122cfd0bf2fc3
minseokkang <aaaa727@khu.ac.kr>
2022-11-09 17:40:35 +0900

    fix : disable SQL

--- src/main/java/com/io/realworld/domain/aggregate/article/repository/ArticleRepository.java
+++ src/main/java/com/io/realworld/domain/aggregate/article/repository/ArticleRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 public interface ArticleRepository extends JpaRepository<Article,Long> {
 
     @EntityGraph(attributePaths = "tagList")
-    @Query("SELECT a FROM Article a JOIN Tag t ON a.id = t.article.id")
+    @Query("SELECT a FROM Article a JOIN Tag t ON a.id = t.article.id ORDER BY a.createdDate DESC")
     List<Article> findByTag(String tag, Pageable pageable);
 
-    @Query("SELECT a FROM Article a WHERE a.author.username = :author")
+    @Query("SELECT a FROM Article a WHERE a.author.username = :author ORDER BY a.createdDate DESC")
     List<Article> findByAuthorName(String author, Pageable pageable);
 
-    @Query("SELECT a FROM Article a LEFT JOIN Favorite f ON f.article.id = a.id WHERE f.user.username =:username")
+    @Query("SELECT a FROM Article a LEFT JOIN Favorite f ON f.article.id = a.id WHERE f.user.username =:username ORDER BY a.createdDate DESC")
     List<Article> findByFavoritedUser(String username, Pageable pageable);
 }
