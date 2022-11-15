4f2b0c9cb2a035428f761fb5c40be59f2cf26502
minseokkang <aaaa727@khu.ac.kr>
2022-11-15 15:32:24 +0900

    fix : baseURL Change

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from "./router";
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://3.35.44.58:8080/'
 const app = createApp(App);
+app.config.globalProperties.axios = axios;
 app.use(router,axios);
 app.mount('#app');
 
